c2bdfaae13360663acd35876eebdd0ee329c4daf
Trieu <trieu@nftwise.com>
2025-10-14 13:16:47 -0600

    Disable react/no-unescaped-entities ESLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,6 +25,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
       "react-hooks/exhaustive-deps": "warn",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
